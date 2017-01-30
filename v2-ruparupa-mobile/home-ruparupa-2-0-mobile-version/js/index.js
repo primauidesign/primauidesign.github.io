adbe28089d1dd80fa9df25d12d60c86f37595eb8
Prima <prima@ruparupa.com>
2017-01-30 15:01:55 +0700

    no message

---
+++ v2-ruparupa-mobile/home-ruparupa-2-0-mobile-version/js/index.js
@@ -0,0 +1,61 @@
+
+    var swiper = new Swiper('.main-slider-banner', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 4500,
+        autoplayDisableOnInteraction: false
+    });
+	var swiper2 = new Swiper('.inspiration-slider', {
+        pagination: '.swiper-pagination2',
+        slidesPerView: 5.5,
+        paginationHide: true,
+        spaceBetween: 16,
+        freeMode: true,
+		breakpoints: {
+            1024: {
+                slidesPerView: 5.5,
+                spaceBetween: 16
+            },
+            768: {
+                slidesPerView: 4.5,
+                spaceBetween: 16
+            },
+            640: {
+                slidesPerView: 2.2,
+                spaceBetween: 5
+            },
+            320: {
+                slidesPerView: 2.2,
+                spaceBetween: 2.5
+            }
+        }
+    });
+    var swiper3 = new Swiper('.people-choice-product', {
+        pagination: '.swiper-pagination3',
+        slidesPerView: 5.5,
+        paginationHide: true,
+        spaceBetween: 16,
+        freeMode: true,
+		breakpoints: {
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 16
+            },
+            768: {
+                slidesPerView:2.2,
+                spaceBetween: 1
+            },
+            640: {
+                slidesPerView: 2.2,
+                spaceBetween: 1
+            },
+            320: {
+                slidesPerView: 2.2,
+                spaceBetween: 2
+            }
+        }
+    });
