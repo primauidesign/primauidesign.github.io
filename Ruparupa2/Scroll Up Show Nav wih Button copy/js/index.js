ae657bb8f610f85b922c579dec99a7f2bf75b797
Prima <prima@ruparupa.com>
2017-02-07 21:30:05 +0700

    no message

--- Ruparupa2/Scroll Up Show Nav wih Button copy/js/index.js
+++ Ruparupa2/Scroll Up Show Nav wih Button copy/js/index.js
@@ -1,12 +1,38 @@
 //Navbar Scroll Event
-var lastScrollTop = 0;
-var navbar        = $('.navbar');
-$(window).scroll(function(event){
-   var st = $(this).scrollTop();
-   if (st > lastScrollTop){
-       navbar.addClass('navbar-scroll-custom');
-   } else {
-      navbar.removeClass('navbar-scroll-custom');
-   }
-   lastScrollTop = st;
-});
+//var lastScrollTop = 0;
+//var navbar        = $('.navbar');
+//$(window).scroll(function(event){
+//   var st = $(this).scrollTop();
+//   if (st > lastScrollTop){
+//       navbar.addClass('navbar-scroll-custom');
+//   } else {
+//      navbar.removeClass('navbar-scroll-custom');
+//   }
+//   lastScrollTop = st;
+//});
+
+var swiper3 = new Swiper('.people-choice-product', {
+        pagination: '.swiper-pagination3',
+        slidesPerView: 5.5,
+        paginationHide: true,
+        spaceBetween: 16,
+        freeMode: true,
+		breakpoints: {
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 16
+            },
+            768: {
+                slidesPerView: 2.2,
+                spaceBetween: 1
+            },
+            640: {
+                slidesPerView: 2.2,
+                spaceBetween: 1
+            },
+            320: {
+                slidesPerView: 2.2,
+                spaceBetween: 2
+            }
+        }
+    });
